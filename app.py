5fc979794b046d77f44e9c37608e5d536ab57d82
Josh Lindssay <joshua.marshall.lindsay@gmail.com>
2019-05-21 01:39:01 -0400

    Cleanup file structure and add comments

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, request, flash, redirect, url_for, session, logging
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
-from wtforms import Form, StringField, PasswordField, SelectField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
 
@@ -8,9 +7,10 @@ from functools import wraps
 app = Flask(__name__)
 app.config.from_object('config.LocalConfig')
 db = SQLAlchemy(app)
-# have to pass in models after since it imports the db object
+# have to import since models relies on db object
 from models import Cities, Users
-
+from forms import RegisterForm
+                    
 
 # custom decorator to verify user is logged in
 def is_logged_in(f):
@@ -23,22 +23,8 @@ def is_logged_in(f):
             return redirect(url_for('login'))
     return wrap
 
-
-class RegisterForm(Form):
-    first = StringField('First Name', [validators.Length(min=1, max=50)])
-    last = StringField('Last Name', [validators.Length(min=1, max=50)])
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField('Email', [validators.Length(min=6, max=50),
-                                  validators.Email(message="Please enter a valid email address")])
-    password = PasswordField('Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message="Passwords do not match")])
-    confirm = PasswordField('Confirm Password')
-    # generate select field of (id, name) or every city in the database
-    city = SelectField('City', coerce=int,
-                       choices=[(c.id, c.name) for c in Cities.query.order_by(Cities.name).all()])
-
-
+# register user with form and validating from wtforms
+# if valid notify user and redirect if successful, otherwise display error
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
@@ -56,27 +42,33 @@ def register():
 
     return render_template('register.html', form=form)
 
-
+# homepage
 @app.route('/')
 def index():
     return render_template('home.html')
 
-
+# login user. does not use wtforms since little validation needs to be done.
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+        # get user information and query database for match
         username = request.form['username']
         password_candidate = request.form['password']
         result = Users.query.filter_by(username=username).first()
+
+        # if info is correct redirect and set session variables
         if result is not None:
             password = result.password
             if sha256_crypt.verify(password_candidate, password):
                 session['logged_in'] = True
                 session['username'] = username
                 session['city'] = result.city
+                # gets the related city name given the users relevant foreign key
                 session['city_name'] = Cities.query.filter_by(id=result.city).first().name
                 flash('Log in successful. Enjoy!', 'success')
                 return redirect(url_for('items'))
+
+            # otherwise return relevant error
             else:
                 return render_template('login.html', error="Invalid password")
         else:
@@ -84,13 +76,13 @@ def login():
 
     return render_template('login.html')
 
-
+# items page, requires that user is logged in
 @app.route('/items')
 @is_logged_in
 def items():
     return render_template('items.html')
 
-
+# logout method, clear session variables and redirect
 @app.route('/logout')
 def logout():
     session.clear()
---
+++ forms.py
@@ -0,0 +1,17 @@
+from wtforms import Form, StringField, PasswordField, SelectField, validators
+from models import Cities
+
+
+class RegisterForm(Form):
+    first = StringField('First Name', [validators.Length(min=1, max=50)])
+    last = StringField('Last Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50),
+                                  validators.Email(message="Please enter a valid email address")])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message="Passwords do not match")])
+    confirm = PasswordField('Confirm Password')
+    # generate select field of (id, name) or every city in the database
+    city = SelectField('City', coerce=int,
+                       choices=[(c.id, c.name) for c in Cities.query.order_by(Cities.name).all()])
